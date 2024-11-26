c6f6132a15489ca489ae49589b2e358bee1e9337
Pervansh <big.bug.cha@gmail.com>
2024-11-26 18:09:01 +0300

    minmod теперь constexpr

--- Hyperbolic_Lab_1/AdditionalMath.h
+++ Hyperbolic_Lab_1/AdditionalMath.h
@@ -8,6 +8,6 @@ inline constexpr int sign(T val) {
 }
 
 template <typename T>
-inline T minmod(T a, T b) {
+inline constexpr T minmod(T a, T b) {
     return T(0.5f) * (sign(a) + sign(b)) * std::fmin(std::fabs(a), std::fabs(b));
 }
